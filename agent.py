e90d922d5b9aaba8cc159dc86833534da8c42165
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-05-11 10:48:14 +0000

    Added interfaces

--- agent.py
+++ agent.py
@@ -2,4 +2,7 @@ from collections import OrderedDict
 import time, os
 curdir = os.path.expanduser("~")
 import numpy as np
-np.set_printoptions(precision=8, suppress=True, linewid
+np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
+import tensorflow as tf
+tf.keras.backend.set_floatx('float64')
+# tf.

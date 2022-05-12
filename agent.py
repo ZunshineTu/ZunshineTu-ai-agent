2c011cfabf1bf14e635d0c2956af49a983d55952
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-05-12 10:36:28 +0000

    update installation docs

--- agent.py
+++ agent.py
@@ -6,4 +6,6 @@ np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
 import tensorflow as tf
 tf.keras.backend.set_floatx('float64')
 # tf.config.run_functions_eagerly(True)
-tf.keras.backend.set_epsilon(tf.experimental.nu
+tf.keras.backend.set_epsilon(tf.experimental.numpy.finfo(tf.keras.backend.floatx()).eps) # 1e-7 default
+import matplotlib.pyplot as plt
+imp

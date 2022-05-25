be2ccc5c8c9d411fae1744e7c668c996b2ef6e8b
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-05-25 17:13:05 +0000

    ChangeLog++

--- agent.py
+++ agent.py
@@ -28,4 +28,5 @@ class GeneralAI(tf.keras.Model):
 
         self.arch, self.env, self.trader, self.env_render, self.save_model = arch, env, trader, env_render, save_model
         self.chkpts, self.max_episodes, self.max_steps, self.learn_rates = tf.constant(chkpts, tf.int32), tf.constant(max_episodes, tf.int32), tf.constant(max_steps, tf.int32), {}
-        for k,v in learn_rates.items(): self.learn_rates[k] = tf.constant(v, compute_dtyp
+        for k,v in learn_rates.items(): self.learn_rates[k] = tf.constant(v, compute_dtype)
+        self.initializer = tf.keras.initializers.GlorotUniform(time.time_n

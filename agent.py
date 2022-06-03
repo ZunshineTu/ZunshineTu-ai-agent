c7afeeef7820464581d08d656687c2f21622167e
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-06-03 12:37:09 +0000

    pic update

--- agent.py
+++ agent.py
@@ -40,4 +40,4 @@ class GeneralAI(tf.keras.Model):
         self.rewards_zero, self.dones_zero = tf.constant([[0]],tf.float64), tf.constant([[False]],tf.bool)
         self.step_zero, self.step_size_one = tf.constant([[0]]), tf.constant([[1]])
 
-        latent_spec = {'dtyp
+        latent_spec = {'dtype':compute_dtype, 'latent_size':latent_size, 'num_latents':1, 'max_latents':aio_max

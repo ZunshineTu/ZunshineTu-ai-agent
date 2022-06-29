ca5ff47baccf0ff506ebb8a0b2c4a72d95df8bfc
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-06-29 10:10:06 +0000

    fixed spacing

--- agent.py
+++ agent.py
@@ -78,4 +78,7 @@ class GeneralAI(tf.keras.Model):
         for loss_group in metrics_loss.values():
             for k in loss_group.keys():
                 if k.endswith('=') or k.endswith('+'): loss_group[k] = [0 for i in range(self.max_episodes)]
-                else: loss_group[k] = [[] for i in ra
+                else: loss_group[k] = [[] for i in range(self.max_episodes)]
+        self.metrics_loss = metrics_loss
+
+    def metrics_update(s

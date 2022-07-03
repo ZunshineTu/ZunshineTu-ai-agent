5da7f09b4d8a81e447a656f07221a7e425066daa
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-07-03 06:45:19 +0000

    Added missing javadoc

--- agent.py
+++ agent.py
@@ -95,4 +95,5 @@ class GeneralAI(tf.keras.Model):
 
 
     def env_reset(self, dummy):
-        obs, info = s
+        obs, info = self.env.reset(); reward, done = 0.0, False
+        if self.env_render: self

bb765dcdfb33f85870527567587fab5c81025bc6
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-09-28 09:43:43 +0000

    update Punjabi Translation

--- agent.py
+++ agent.py
@@ -222,4 +222,5 @@ class GeneralAI(tf.keras.Model):
             util.stats_update(self.action.stats['rwd'], rewards_total); avg, ma, ema, snr, std = util.stats_get(self.action.stats['rwd'])
 
             self.reset_states(); loss = self.PG_learner_onestep(outputs)
-            util.stats_update(self.action.stats['loss'], tf.math.reduce_mean(loss['action_lik'])); avg_loss, ma_loss, ema_loss, snr_loss, std_loss = util.stats_get(self.action.stats[
+            util.stats_update(self.action.stats['loss'], tf.math.reduce_mean(loss['action_lik'])); avg_loss, ma_loss, ema_loss, snr_loss, std_loss = util.stats_get(self.action.stats['loss'])
+            self.action.optimizer['action'].learning_rate = self.learn_rates['action

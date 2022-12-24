adf2a85aa968a0cb6037404ef0b2a22088f9a762
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-12-24 20:36:32 +0000

    Add functional tests

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -2,3 +2,5 @@ from collections import OrderedDict
 import time
 import multiprocessing as mp
 import numpy as np
+np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
+# np.random.see

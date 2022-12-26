8604d34bc9e36ab7525340ed3c12951b5c11af8c
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2022-12-26 11:13:24 +0000

    update lockfile

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -3,4 +3,10 @@ import time
 import multiprocessing as mp
 import numpy as np
 np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
-# np.random.see
+# np.random.seed(0)
+import gymnasium as gym
+import gym_util
+
+
+class AsyncWrapperEnv(gym.Env):
+    metadat

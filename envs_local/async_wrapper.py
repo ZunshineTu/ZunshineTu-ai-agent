21bc769fac909dabed4a95cf4104f02b9a0b9589
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-01-07 11:38:48 +0000

    PARISC: fix user-triggerable panic on parisc

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -33,4 +33,5 @@ class AsyncWrapperEnv(gym.Env):
             self.action_space.spaces['timedelta'] = gym.spaces.Box(low=0.0, high=np.inf, shape=(1,), dtype=np.float64)
             self.action_space.spaces['origspace'] = env.action_space
             if np_struc: self.action_dtype = np.dtype([('timedelta', 'f8'), ('origspace', env.action_dtype)])
-        if not (isinstance(env.observ
+        if not (isinstance(env.observation_space, gym.spaces.Dict) and 'timestamp' in env.observation_space.spaces):
+     

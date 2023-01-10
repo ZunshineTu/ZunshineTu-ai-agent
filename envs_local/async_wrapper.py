c34592d41a03f61ed386c36c789cb34f73995528
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-01-10 09:48:01 +0000

    Miscellaneous fixes

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -37,4 +37,5 @@ class AsyncWrapperEnv(gym.Env):
             self._obs_timing = True
             self.observation_space = gym.spaces.Dict()
             self.observation_space.spaces['timestamp'] = gym.spaces.Box(low=0.0, high=np.inf, shape=(1,), dtype=np.float64)
-            self.observation_spac
+            self.observation_space.spaces['origspace'] = env.observation_space
+            if np_struc: self.obs_dtype 

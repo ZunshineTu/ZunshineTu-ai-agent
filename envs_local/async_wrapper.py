cb31a2cce2a02c4dc1fa3bcb28b6121a4d3ba3ee
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-02-03 13:22:43 +0000

    Release 2.0.0

--- envs_local/async_wrapper.py
+++ envs_local/async_wrapper.py
@@ -59,4 +59,6 @@ class AsyncWrapperEnv(gym.Env):
 
         # self._lock_print = mp.Lock()
         self._proc_ctrl = mp.Value('b', 0) # 1 = close, 0 = reset, -1 = step, -2 = done
-        self._action_shared = mp.sharedctypes.Array('B', act
+        self._action_shared = mp.sharedctypes.Array('B', action_size)
+        self._obs_shared = mp.sharedctypes.Array('B', obs_size)
+        se

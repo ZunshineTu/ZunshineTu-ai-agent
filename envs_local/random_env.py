5b73075bd68ec5f50a61fb3d91c3f955e4b145f9
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-06-04 07:10:50 +0000

    travis-ci

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -1,4 +1,7 @@
 import numpy as np
 np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
 # np.random.seed(0)
-impo
+import gymnasium as gym
+import gym_util
+
+# TODO auto make the dtype struc from space for nu

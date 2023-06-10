cb08f73a6cda08080a3009c388b5e8c678e4a950
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-06-10 08:39:58 +0000

    more doc work

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -4,4 +4,9 @@ np.set_printoptions(precision=8, suppress=True, linewidth=400, threshold=100)
 import gymnasium as gym
 import gym_util
 
-# TODO auto make the dtype struc from space for numpy dtype compatability with gym, need to include space it has more info like low,h
+# TODO auto make the dtype struc from space for numpy dtype compatability with gym, need to include space it has more info like low,high
+# def gym_space_to_dtype(space):
+#     pass
+
+class RandomEnv(gym.Env):
+    metadata =

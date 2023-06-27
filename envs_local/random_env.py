a2713bbc835058109aa395f9a6f8c802f9af3108
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-06-27 13:15:46 +0000

    Update MliBotController.java

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -18,4 +18,6 @@ class RandomEnv(gym.Env):
         # self.obs_zero = gym.spaces.flatten(self.observation_space, self.observation_space.sample())
         # self.action_spec, self.action_zero, self.action_zero_out = gym_util.get_spec(self.action_space)
         # self.obs_spec, self.obs_zero, self.obs_zero_out = gym_util.get_spec(self.observation_space)
-        # obs_dtype = gym_space_to_dtype(self.observation_sp
+        # obs_dtype = gym_space_to_dtype(self.observation_space)
+        # action_smpl = self.action_space.sample()
+        # obs_smpl = sel

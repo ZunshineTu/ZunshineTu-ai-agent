ec75ea8be18a8e3f4ef81d169b86828e81682888
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-07-29 18:51:21 +0000

    (maint) formatting

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -52,4 +52,7 @@ class RandomEnv(gym.Env):
         #     'six': gym.spaces.Discrete(6), # int
         #     'bin': gym.spaces.MultiBinary(6), # np.ndarray([1, 0, 1, 0, 0, 1], dtype=int8)
         #     'mul': gym.spaces.MultiDiscrete([6,2]), # np.ndarray([3, 0], dtype=int64)
-        #     'val': gym.spaces.Box(low=0, high=np.inf, shape=(), dtype=np.float64)} # np.ndarray(2.2274436
+        #     'val': gym.spaces.Box(low=0, high=np.inf, shape=(), dtype=np.float64)} # np.ndarray(2.22744361)
+        # )
+        # return gym.spaces.Discrete(6)
+        # return gym.spaces.Mult

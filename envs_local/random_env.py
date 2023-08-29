267b9949b313bef42b65d0e2edc729ff6263cd02
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2023-08-29 10:22:40 +0000

    Typo [ci skip]

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -83,4 +83,5 @@ class RandomEnv(gym.Env):
         action_space.spaces['dt_sub'].spaces['byte'] = gym.spaces.Box(low=0, high=255, shape=(2,), dtype=np.uint8)
 
         action_space.spaces['byte'] = gym.spaces.Box(low=0, high=255, shape=(1,), dtype=np.uint8)
-        action_space.s
+        action_space.spaces['2Darray'] = gym.spaces.Box(low=0, high=255, shape=(2,3), dtype=np.uint8)
+        actio

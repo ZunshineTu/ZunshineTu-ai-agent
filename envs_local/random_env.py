f13dc7ac7c6180d0782cb3694640c941f13e3a91
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2024-01-09 15:03:43 +0000

    tcp_cubic: better follow cubic curve after idle period

--- envs_local/random_env.py
+++ envs_local/random_env.py
@@ -235,4 +235,6 @@ if __name__ == '__main__':
         test_spec, test_obs_zero, test_obs_zero_out = gym_util.get_spec(env.observation_space, compute_dtype='float32')
         test_obs = gym_util.space_to_feat(obs, env.observation_space)
 
-        action = env.action_space.
+        action = env.action_space.sample()
+        # test_out = [np.asarray([2],np.int64), np.asarray([3],np.int64)]
+        test_ou

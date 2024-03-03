c88ab0f9c9df5df13b6a7ebba91265c385e206b5
ZunshineTu <159596001+ZunshineTu@users.noreply.github.com>
2024-03-03 06:40:15 +0000

    Delete _icons.scss

--- envs_local/reconfig_wrapper.py
+++ envs_local/reconfig_wrapper.py
@@ -36,4 +36,5 @@ class ReconfigWrapperEnv(gym.Env):
 
         self.reconfig_obs = False
         if isinstance(env.observation_space, gym.spaces.Box) and env.observation_space.shape[-1] > 1:
-            self.
+            self.reconfig_obs = True
+            num_feat_obs = env.observation_space.shape[-1]; self.num_feat_
